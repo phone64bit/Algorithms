feb19ee4b68825c82a01e05e2a410ce076620b8c
phone64bit <66268439+phone64bit@users.noreply.github.com>
2023-05-10 18:19:36 +0700

    O(N*logN)

---
+++ coornidate_compression.cpp
@@ -0,0 +1,23 @@
+// Coornidate Compression by phone64bit
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> number = {5, 1, 3, 9, 4};
+vector<pair<int, int>> val = {{5, 0}, {1, 1}, {3, 2}, {9, 3}, {4, 4}};
+// First Element is vector value and Second Element is vector idx.
+
+int main() {
+    sort(val.begin(), val.end()); // We sort the pair vector
+    vector<int> cc(val.size()); // Initialize the coornidate compression vector to get the idx in O(1)
+    for(int i = 0, idx = 0; i < val.size(); i++) {
+        if( i > 0 && val[i].first != val[i-1].first ) idx++; // If previous vector value is as same as current vector value then it's the same value.
+        cc[val[i].second] = idx;
+    };
+    for(int i=0; i<number.size(); i++) { 
+        printf("%d\t->\t%d\n", number[i], cc[i]);
+    };
+    return 0;
+};
+
+// Summary : Time complexity is O(N*logN) [Worst case is O(N^2)] for sorting vector value and O(N) for building coornidate compression vector
+//           Space complexity is O(N)
